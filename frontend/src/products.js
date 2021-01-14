f3d7e1b8124a9f9792a39b7727fabead1b70c993
Manish Kumar <manishmania20@gmail.com>
2021-01-14 13:25:42 +0530

    added-products

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Assassins\'s Creed: Valhalla',
+    image: '/images/acvalhalla.jpg',
+    description:
+      'Assassin\'s Creed: Valhalla is the twelfth main installment in the Assassin\'s Creed series developed by Ubisoft. In the game, the player takes control of Eivor, a Viking who fought in the Norse invasion of England.',
+    brand: 'Ubisoft',
+    category: 'Action RPG',
+    price: 59.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Immortals: Fenyx Rising',
+    image: '/images/fenyx-rising.jpg',
+    description:
+      'Immortals: Fenyx Rising is an action adventure game by Ubisoft. Play as Fenyx, a new winged demigod, on a quest to save the Greek gods and their home from a dark curse. Take on mythological beasts, master the legendary powers of the gods, and defeat Typhon, the deadliest Titan in Greek mythology, in an epic fight for the ages.',
+    brand: 'Ubisoft',
+    category: 'Action Adventure',
+    price: 59.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Hitman: 3',
+    image: '/images/hitman3.jpg',
+    description:
+      'Hitman: 3 is the 8th and latest installment to the Hitman video game series.The game will serve as the conclusion to the World of Assassination trilogy. It will launch with 6 new locations to explore, and owners of Hitman and Hitman: 2 will be able to import locations introduced in those games into Hitman: 3. ',
+    brand: 'Io-Interactive',
+    category: 'Action Stealth',
+    price: 59.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Little Nightmares: 2',
+    image: '/images/little-nightmares.jpg',
+    description:
+      'Little Nightmares II is an upcoming video game, set to be released in February 11th, 2021 for PS, Xbox, Switch, PC, and Stadia. Players will take control of Mono, with Six as an AI-controlled companion character, as the two journey to the Black Signal Tower.',
+    brand: 'Tarsier Studios',
+    category: 'Puzzle Platformer Horror',
+    price: 15.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Riders: Republic',
+    image: '/images/riders-republic.jpg',
+    description:
+      'Riders Republic is an upcoming sports video game developed by Ubisoft Annecy and published by Ubisoft. The five main activities available in the game include mountain biking, skiing, snowboarding, wingsuit flying, and rocket wingsuiting.',
+    brand: 'Ubisoft',
+    category: 'Sports',
+    price: 59.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'The Medium',
+    image: '/images/the-medium.jpg',
+    description:
+      'Discover a dark mystery only a medium can solve. Explore the real world and the spirit world at the same time. Use your psychic abilities to solve puzzles spanning both worlds, uncover deeply disturbing secrets, and survive encounters with The Maw - an entity born from an unspeakable tragedy.',
+    brand: 'Bloober',
+    category: 'Psychological Horror',
+    price: 35.99,
+    countInStock: 0,
+    rating: 5.0,
+    numReviews: 12,
+  },
+]
+
+export default products
