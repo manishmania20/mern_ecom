6cd43be49f7261b5e7db09824cb7a8df66ffc4b3
Manish Kumar <manishmania20@gmail.com>
2021-03-07 19:14:31 +0530

    loginlogoutprofile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,4 @@
-import { authUser, registerUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
 import express from 'express'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -6,6 +6,6 @@ const router = express.Router()
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get( protect, getUserProfile )
+router.route('/profile').get( protect, getUserProfile ).put( protect, updateUserProfile )
 
 export default router
